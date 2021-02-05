de0437a80a19beaac4d0d1685ff0f7296949589c
Exploit89 <khudajbergenov89@gmail.com>
2021-02-05 11:34:56 +1000

    Commit from PC-Work

--- SQLTablework(temp).py
+++ SQLTablework(temp).py
@@ -6,8 +6,15 @@ connect = sqlite3.connect(database)
 cursor = connect.cursor()
 
 cursor.execute("""
-    CREATE TABLE Ship(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL    
+CREATE TABLE Academy(
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+Category	TEXT	,
+SubCategory	TEXT	,
+Name	TEXT	,
+Tier	TEXT	,
+Rank	TEXT	,
+FOREIGN KEY (id) REFERENCES Ship (id)
+)
 """)
 
 names = next(zip(*cursor.description))

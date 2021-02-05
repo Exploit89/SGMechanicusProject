49117937a4adef5f65c198154bf932e06c95054f
Exploit89 <khudajbergenov89@gmail.com>
2021-02-05 10:08:11 +1000

    PC-Home commit

--- SQLTablework(temp).py
+++ SQLTablework(temp).py
@@ -6,7 +6,11 @@ connect = sqlite3.connect(database)
 cursor = connect.cursor()
 
 cursor.execute("""
-    SELECT * FROM Ship 
+    INSERT INTO Ship (Nation, Type, Tier, Name, Role1, TacticalComponent, Superdevice, ProcessorCapacity, PowerCapacity,
+    EnergyCapacity, AutoEnergyRecovery, ComponentSlots, Velocity, Agility, ShieldResEM, ShieldResKinetic, 
+    ShieldResThermal, ArmorResEM, ArmorResKinetic, ArmorResThermal, Shield, AutoShieldRecovery, Armor,
+    AutoArmorRecovery, VolumeFactor, WarpStability, CargoSpaceCapacity, Volume)
+    VALUES ('ECD', 'Frigate', 'T1', 'Mist')
 """)
 
 names = next(zip(*cursor.description))

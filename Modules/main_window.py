f6dd89f6082ce7751ce7e1ca48df9a41803f7900
Exploit89 <khudajbergenov89@gmail.com>
2021-02-19 17:43:27 +1000

    Commit from PC-Work

--- Modules/main_window.py
+++ Modules/main_window.py
@@ -1,5 +1,6 @@
 # Главное окно
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QGridLayout, QAction, QVBoxLayout
 import sys
 
 
@@ -22,6 +23,28 @@ class MainWindow(QtWidgets.QMainWindow):
         x = (desktop.width() - main_window.frameSize().width()) // 2
         y = (desktop.height() - main_window.frameSize().height()) // 2
         self.move(x, y)
+
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu("File")
+        exit_action = QAction("Quit", self)
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
+
+        toolbar = self.addToolBar("something")  # блок панели инструментов (кнопки настроек профиля)
+        toolbar.setMovable(False)
+        toolbar.setFloatable(False)
+        toolbar.setFixedHeight(30)
+
+        grid = QGridLayout()
+        self.setLayout(grid)
+        #central_widget = MainWindow(self)
+        #self.setCentralWidget(central_widget)  # не фурычит
+
+        shiptree = QtWidgets.QVBoxLayout()
+        grid.addLayout(shiptree, 0, 0)
+
+        button1 = QtWidgets.QPushButton("button1")
+        shiptree.addWidget(button1, 0)
 
 
 if __name__ == "__main__":

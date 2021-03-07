742c1b761458aad60bb88802b97f374dea8ec9a1
Exploit89 <khudajbergenov89@gmail.com>
2021-03-07 22:58:38 +1000

    PC-Home commit

---
+++ Modules/descriptionlist.py
@@ -0,0 +1,72 @@
+# Список деталей
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+from Modules import styles
+from PyQt5.QtWidgets import *
+
+
+class DescriptionView(QtWidgets.QVBoxLayout):
+
+    def __init__(self, parent=None):
+        QtWidgets.QVBoxLayout.__init__(self, parent)
+
+        self.damage_label = QLabel("Damage / DPS", alignment=Qt.AlignHCenter)
+        self.damage = QtWidgets.QHBoxLayout()
+        self.totaldamagelabel = QLabel("Damage: ", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.totaldamagevalue = QLabel("100500", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.dpsdamagelabel = QLabel("DPS: ", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.dpsdamagevalue = QLabel("100500", alignment=Qt.AlignLeft | Qt.AlignTop)
+
+        self.resistance_label = QLabel("Resistance", alignment=Qt.AlignHCenter)
+        self.shield_resistance = QtWidgets.QHBoxLayout()
+        self.shield_resistance_graph = QtWidgets.QHBoxLayout()
+        self.armor_resistance_graph = QtWidgets.QHBoxLayout()
+
+        self.shield_resistance_label = QLabel("Shield: ", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.em_shield_label = QLabel("EM", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.em_shieldvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.thermal_shield_label = QLabel("Thermal", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.thermal_shieldvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.kinetic_shield_label = QLabel("Kinetic", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.kinetic_shieldvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+
+        self.armor_resistance_label = QLabel("Armor: ", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.em_armorvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.thermal_armorvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.kinetic_armorvalue = QLabel("0%", alignment=Qt.AlignLeft | Qt.AlignTop)
+
+
+
+"""блок исключительно для теста"""
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.my_tree_view = DescriptionView()
+        self.my_tree_view.clicked.connect(self.on_tree_view_click)
+
+        self.text_edit = QPlainTextEdit()
+
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.my_tree_view)
+        main_layout.addWidget(self.text_edit)  # виджет, куда нужно отправить данные
+
+        self.setLayout(main_layout)
+
+    def on_tree_view_click(self, index):
+        """Добавляет item туда, куда нужно"""
+        item = self.my_tree_view.get_item(index)
+        self.text_edit.appendPlainText(item.text())  # добавление в конец списка
+        print(item.text())  # вывод в консоль
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
--- Modules/mainwidget.py
+++ Modules/mainwidget.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel
 
-from Modules import shiplist, equipmentlist, styles, fitlist, ships_tuples
+from Modules import shiplist, equipmentlist, styles, fitlist, ships_tuples, descriptionlist
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -22,23 +22,23 @@ class MainWidget(QtWidgets.QWidget):
         self.shiptreebox = shiplist.ShipTreeView()  # определяем виджет для добавления списка шипов
         self.shiptreebox.clicked.connect(self.on_tree_view_click)
 
-        fittreebox = fitlist.FitTreeView()  # определяем виджет для добавления списка фитов
+        self.fittreebox = fitlist.FitTreeView()  # определяем виджет для добавления списка фитов
+
+        ship_tab = QtWidgets.QTabWidget()
+        ship_tab.addTab(self.shiptreebox, "Ship")  # страница шипов
+        ship_tab.addTab(self.fittreebox, "Fit")  # страница фитов
+        shiptree.addWidget(ship_tab)  # добавляем список шипов
+        ship_tab.setCurrentIndex(0)
+        ship_tab.setStyleSheet(styles.tab_style)
 
         equipmenttree = QtWidgets.QVBoxLayout()  # Дерево эквипа
         frame_equipmenttree = QtWidgets.QFrame()  # Рамка
         frame_equipmenttree.setFixedSize(200, 230)
         frame_equipmenttree.setLayout(equipmenttree)
         grid.addWidget(frame_equipmenttree, 10, 1, 4, 1)
-        equipmenttreebox = equipmentlist.EquipmentTreeView()
+        self.equipmenttreebox = equipmentlist.EquipmentTreeView()
 
-        ship_tab = QtWidgets.QTabWidget()
-        ship_tab.addTab(self.shiptreebox, "Ship")  # страница шипов
-        ship_tab.addTab(fittreebox, "Fit")  # страница фитов
-        shiptree.addWidget(ship_tab)  # добавляем список шипов
-        ship_tab.setCurrentIndex(0)
-        ship_tab.setStyleSheet(styles.tab_style)
-
-        equipmenttree.addWidget(equipmenttreebox)  # Добавляем кнопку
+        equipmenttree.addWidget(self.equipmenttreebox)  # Добавляем кнопку
 
         main_image_frame = QtWidgets.QFrame()  # рамка для главной картинки
         main_image_frame.setFixedSize(250, 250)
@@ -58,10 +58,37 @@ class MainWidget(QtWidgets.QWidget):
 
         main_image_frame.setLayout(vmain_image)
 
+        self.descriptiontree = descriptionlist.DescriptionView()
         description_frame = QtWidgets.QFrame()  # рамка для показателей
         description_frame.setFixedSize(300, 500)
         description_frame.setFrameStyle(QtWidgets.QFrame.StyledPanel)
+        description_frame.setLayout(self.descriptiontree)
         grid.addWidget(description_frame, 2, 12, 12, 1)
+
+        self.descriptiontree.addWidget(self.descriptiontree.damage_label)
+        self.descriptiontree.addLayout(self.descriptiontree.damage, QtCore.Qt.AlignTop)
+        self.descriptiontree.damage.addWidget(self.descriptiontree.totaldamagelabel)
+        self.descriptiontree.damage.addWidget(self.descriptiontree.totaldamagevalue)
+        self.descriptiontree.damage.addWidget(self.descriptiontree.dpsdamagelabel)
+        self.descriptiontree.damage.addWidget(self.descriptiontree.dpsdamagevalue)
+
+        self.descriptiontree.addWidget(self.descriptiontree.resistance_label)
+        self.descriptiontree.addLayout(self.descriptiontree.shield_resistance, QtCore.Qt.AlignTop)
+        self.descriptiontree.shield_resistance.addWidget(QLabel(""))
+        self.descriptiontree.shield_resistance.addWidget(self.descriptiontree.em_shield_label)
+        self.descriptiontree.shield_resistance.addWidget(self.descriptiontree.thermal_shield_label)
+        self.descriptiontree.shield_resistance.addWidget(self.descriptiontree.kinetic_shield_label)
+        self.descriptiontree.addLayout(self.descriptiontree.shield_resistance_graph, QtCore.Qt.AlignTop)
+        self.descriptiontree.shield_resistance_graph.addWidget(self.descriptiontree.shield_resistance_label)
+        self.descriptiontree.shield_resistance_graph.addWidget(self.descriptiontree.em_shieldvalue)
+        self.descriptiontree.shield_resistance_graph.addWidget(self.descriptiontree.thermal_shieldvalue)
+        self.descriptiontree.shield_resistance_graph.addWidget(self.descriptiontree.kinetic_shieldvalue)
+        self.descriptiontree.addLayout(self.descriptiontree.armor_resistance_graph, QtCore.Qt.AlignTop)
+        self.descriptiontree.armor_resistance_graph.addWidget(self.descriptiontree.armor_resistance_label)
+        self.descriptiontree.armor_resistance_graph.addWidget(self.descriptiontree.em_armorvalue)
+        self.descriptiontree.armor_resistance_graph.addWidget(self.descriptiontree.thermal_armorvalue)
+        self.descriptiontree.armor_resistance_graph.addWidget(self.descriptiontree.kinetic_armorvalue)
+
 
         superdevice_frame = QtWidgets.QFrame()  # рамка для супердевайса
         superdevice_frame.setFixedSize(50, 50)
@@ -138,6 +165,21 @@ class MainWidget(QtWidgets.QWidget):
         device_frame3.setFrameStyle(QtWidgets.QFrame.StyledPanel)
         grid.addWidget(device_frame3, 12, 9, 1, 1)
 
+        ammo_frame1 = QtWidgets.QFrame()
+        ammo_frame1.setFixedSize(50, 50)
+        ammo_frame1.setFrameStyle(QtWidgets.QFrame.StyledPanel)
+        grid.addWidget(ammo_frame1, 14, 7, 1, 1)
+
+        ammo_frame2 = QtWidgets.QFrame()
+        ammo_frame2.setFixedSize(50, 50)
+        ammo_frame2.setFrameStyle(QtWidgets.QFrame.StyledPanel)
+        grid.addWidget(ammo_frame2, 14, 8, 1, 1)
+
+        ammo_frame3 = QtWidgets.QFrame()
+        ammo_frame3.setFixedSize(50, 50)
+        ammo_frame3.setFrameStyle(QtWidgets.QFrame.StyledPanel)
+        grid.addWidget(ammo_frame3, 14, 9, 1, 1)
+
         grid.setColumnMinimumWidth(0, 1)  # ширина пустой колонки
         grid.setColumnMinimumWidth(2, 20)
         grid.setColumnMinimumWidth(6, 5)
@@ -176,20 +218,19 @@ class MainWidget(QtWidgets.QWidget):
         device_slot_label = QtWidgets.QLabel("Devices")
         grid.addWidget(device_slot_label, 11, 7, 1, 3, QtCore.Qt.AlignCenter | QtCore.Qt.AlignBottom)
 
-        self.setLayout(grid)
+        ammo_slot_label = QtWidgets.QLabel("Ammo")
+        grid.addWidget(ammo_slot_label, 13, 7, 1, 3, QtCore.Qt.AlignCenter | QtCore.Qt.AlignBottom)
 
-        # self.my_tree_view = ShipTreeView()
-        # self.my_tree_view.clicked.connect(self.on_tree_view_click)
+        self.setLayout(grid)
 
     def on_tree_view_click(self, index):
         item = self.shiptreebox.get_item(index)
         pointerQStandardItem = self.shiptreebox.ship_standard_item_model.itemFromIndex(index)
-        print(f'Вы кликнули -> {item.text():>8}, --> pointerQStandardItem -> {pointerQStandardItem}')
+        print(f'{item.text():>5}, {pointerQStandardItem}')  # pointerQStandardItem | :>5 это размер отступа
         print(item.text())  # Имя объекта
         print(item.row())  # Номер строки объекта (возможно надо еще поработать, чтобы не было конфликтов)
         # print(item.flat)
 
-        self.image_label.setText(item.text())  # Задаем название шипа над картинкой
         shiptuple = ships_tuples.allships_parts  # словарь с кортежами шипов
         shiptuple2 = ships_tuples.allships  # кортеж с кортежами шипов
 
@@ -197,12 +238,11 @@ class MainWidget(QtWidgets.QWidget):
         itemrow = int(item.row())  # приводим номер строки объекта в число (получить через условия конкретный индекс)
 
         if itemname in shiptuple:
-            print('that is!!')
             self.newpixmap = QPixmap(shiptuple2[itemrow][9])
             self.imagelabel.setPixmap(self.newpixmap)
+            self.image_label.setText(item.text())  # Задаем название шипа над картинкой
         else:
             pass
-
 
     def get_data_profile(self):
         """получение данных для последующей записи в файл"""
